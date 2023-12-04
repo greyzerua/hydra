2061a1c98d89855c98faf1ff0faf1793e75627bc
Serhii <greyzerua@ukr.net>
2023-12-04 13:40:15 +0200

    add animantion

--- app/js/scroll-to-section.js
+++ app/js/scroll-to-section.js
@@ -12,21 +12,42 @@ const scrollToSectionByLink = (event) => {
   }
 };
 
+const elementsConfig = [
+  {
+    elementSelector: '.how-to__build',
+    animateOnce: true
+  },
+  {
+    elementSelector: '.description__wrapper'
+  },
+  {
+    elementSelector: '.about-us__items'
+  },
+  {
+    elementSelector: '.introduction__content'
+  }
+]
+
 
 function onEntry(entry) {
   entry.forEach(change => {
+    const className = change.target.className;
+    const elemConfig = elementsConfig.find(el => className.includes(el.elementSelector.substring(1)))
+    const shouldRemoveClass = !change.isIntersecting && elemConfig.isAnimated && !elemConfig.animateOnce;
     if (change.isIntersecting) {
+      if (elemConfig) {
+        elemConfig.isAnimated = true;
+      }
       change.target.classList.add('element-show');
-      change.target.classList.add('element-hide');
-    } else {
+    } else if (shouldRemoveClass) {
       change.target.classList.remove('element-show');
     }
   });
 }
 
-let options = { threshold: [0.5] };
+let options = { threshold: [0, 0.9] };
 let observer = new IntersectionObserver(onEntry, options);
-let elements = document.querySelectorAll('.how-to__build, .description__wrapper, .about-us__items');
+let elements = document.querySelectorAll(elementsConfig.map(el => el.elementSelector).join());
 
 for (let elm of elements) {
   observer.observe(elm);
